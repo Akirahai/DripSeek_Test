e0b026b6c88a2595f55d7fe7dff96ae893c88d52
Hai Le <leviethai03122004@gmail.com>
2025-06-15 09:17:45 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,11 +16,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${inter.variable}`} suppressHydrationWarning={true}>
+    <html lang="en" className={`${inter.variable}`}>
       <head>
         {/* Google Fonts link for Inter is handled by next/font */}
       </head>
-      <body className="font-body antialiased min-h-screen flex flex-col">
+      <body className="font-body antialiased min-h-screen flex flex-col" suppressHydrationWarning={true}>
         {children}
         <Toaster />
       </body>
